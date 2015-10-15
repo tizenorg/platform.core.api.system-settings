77c9ea3b0cd40b8e6fb2506248997411efdc8f70
MyoungJune Park <mj2004.park@samsung.com>
2015-10-15 13:47:34 +0900

    change tzpath link for timezone handling
    Change-Id: Ie62a04fcb54d94b448afaf187c58fcc830244fb5
    Signed-off-by: DongHun Kwak <dh0128.kwak@samsung.com>

--- src/system_setting_platform.c
+++ src/system_setting_platform.c
@@ -74,7 +74,7 @@
 
 #define SETTING_TIME_ZONEINFO_PATH 		"/usr/share/zoneinfo/"
 #define SETTING_TIME_SHARE_LOCAL_PATH 	"/usr/share/locale"
-#define SETTING_TZONE_SYMLINK_PATH		"/opt/etc/localtime"
+#define SETTING_TZONE_SYMLINK_PATH		"/etc/localtime"
 
 
 static char *_get_cur_font();
